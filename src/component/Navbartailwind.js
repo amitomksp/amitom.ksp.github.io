f7fdd08d8c70b20950e8cac99504d3751dfb14ac
amit om kashyap <amit.ece.21@nitap.ac.in>
2024-08-22 23:31:59 +0530

    navbar change

--- src/component/Navbartailwind.js
+++ src/component/Navbartailwind.js
@@ -1,9 +1,10 @@
 import React from "react";
+
 import { Fragment, useState } from 'react'
 import { Dialog, Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon,XMarkIcon } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
-
+import { useLocation } from 'react-router-dom';
 const navigation = [
     { name: 'HOME', href: '/', current: true },
     { name: 'Skills', href: '/skills', current: false },
@@ -16,14 +17,16 @@ const navigation = [
   }
 
 function Navbartl(){
+  const location = useLocation();
     return (
         <Disclosure as="nav" className="bg-gray-800 sticky top-0 z-50">
         {({ open }) => (
           <>
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
               <div className="relative flex h-16 items-center justify-between">
+
+                 {/* sm:hidden for Mobile menu button*/}
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                  {/* Mobile menu button*/}
                   <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="absolute -inset-0.5" />
                     <span className="sr-only">Open main menu</span>
@@ -36,26 +39,25 @@ function Navbartl(){
                 </div>
                 <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                   <div className="flex flex-shrink-0 items-center">
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                      alt="Your Company"
-                    />
-                  </div>
+                   
+            </div>
+                      {/* sm:block for desktop menu button*/}
                   <div className="hidden sm:ml-6 sm:block">
                     <div className="flex space-x-4">
                       {navigation.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className={classNames(
-                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                            'rounded-md px-3 py-2 text-sm font-medium'
-                          )}
-                          aria-current={item.current ? 'page' : undefined}
-                        >
-                          {item.name}
-                        </a>
+                         <a
+                         key={item.name}
+                         href={item.href}
+                         className={classNames(
+                           location.pathname === item.href
+                             ? 'bg-gray-900 text-white'
+                             : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                           'rounded-md px-3 py-2 text-sm font-medium'
+                         )}
+                         aria-current={location.pathname === item.href ? 'page' : undefined}
+                       >
+                         {item.name}
+                       </a>
                       ))}
                     </div>
                   </div>
@@ -129,21 +131,23 @@ function Navbartl(){
                 </div>
               </div>
             </div>
-  
+  {/*sm:hidden for mobile navigation */}
             <Disclosure.Panel className="sm:hidden">
               <div className="space-y-1 px-2 pb-3 pt-2">
                 {navigation.map((item) => (
                   <Disclosure.Button
-                    key={item.name}
-                    as="a"
-                    href={item.href}
-                    className={classNames(
-                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'block rounded-md px-3 py-2 text-base font-medium'
-                    )}
-                    aria-current={item.current ? 'page' : undefined}
-                  >
-                    {item.name}
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    location.pathname === item.href
+                      ? 'bg-gray-900 text-white'
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={location.pathname === item.href ? 'page' : undefined}
+                >
+                  {item.name}
                   </Disclosure.Button>
                 ))}
               </div>
